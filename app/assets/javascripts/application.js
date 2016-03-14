7e71a93a4f54e3321482f942648c0d2ad4caedac
chinmay-amuratech <chinmay@amuratech.com>
2016-03-14 15:45:31 +0530

    Basics for application initialized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
+//= require underscore
+//= require backbone
+// require_tree .
