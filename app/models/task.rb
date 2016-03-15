cf9a85e560a17d791f6ade6e98b67fb0ec85b9bb
chinmay-amuratech <chinmay@amuratech.com>
2016-03-15 18:14:37 +0530

    Fixes - todos -> tasks for controller and models

---
+++ app/models/task.rb
@@ -0,0 +1,14 @@
+class Task
+  include Mongoid::Document
+
+  field :name, type: String
+  field :flag, type: Boolean, default: true
+
+  def as_json(options = {})
+    json = super(options)
+    json["id"] = self.id.to_s
+    json.delete "_id"
+    json
+  end
+
+end
